8827a433a2b59e1d917589054bf1630323aa6d36
anastasia <anastasia@hurto>
2025-02-25 10:47:49 +0300

    changed

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,11 @@ public class Main {
         readers[2] = new Reader("Иванов И. И.", "003", "Факультет физики", "03.03.2000", "1122334455");
         readers[3] = new Reader("Кузнецов К. К.", "004", "Факультет химии", "04.04.2000", "2233445566");
         readers[4] = new Reader("Смирнов С. С.", "005", "Факультет биологии", "05.05.2000", "3344556677");
-
-        readers[0].TakeBook(2);
-        readers[1].TakeBook("Капитанская дочка", "Ревизор", "Психология");
-        readers[2].ReturnBook(4);
-        readers[3].ReturnBook("Психология", "Шинель");
+        for (Reader reader : readers) {
+            reader.TakeBook(2);
+            reader.TakeBook("Капитанская дочка", "Ревизор", "Психология");
+            reader.ReturnBook(4);
+            reader.ReturnBook("Психология", "Шинель");
+        }
     }
 }
